8ebcae1fdac5f0cc3f0cbe0b9423619b5a951241
naufalDev10 <naufalmobilelegends1@gmail.com>
2025-02-16 19:28:50 +0700

    add about us section and framer motion

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,6 +4,7 @@ import { Pagination, Autoplay, EffectFade } from "swiper/modules";
 import "swiper/swiper-bundle.css";
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const heroImage = [
   {
@@ -37,17 +38,17 @@ export default function Home() {
         pagination={{ clickable: true }}
         effect="fade"
         fadeEffect={{ crossFade: true }}
-        className="relative mt-[3.6rem] md:mt-[4.5rem] md:w-full md:h-screen"
+        className="relative mt-[3.6rem] md:mt-[4.5rem] md:w-full md:h-auto sm:h-auto lg:h-auto xl:h-screen"
       >
         {heroImage.map((slide, index) => (
           <SwiperSlide key={index}>
             <div className="relative">
               <img
                 src={slide.image}
-                alt=""
+                alt="image"
                 className="w-full h-[350px] md:w-full md:h-full"
               />
-              <div className="text-black w-full px-6 py-6 md:absolute md:top-[35%] md:left-20 md:max-w-md md:text-white md:py-0 md:px-0">
+              <div className="text-black w-full px-6 py-6 md:absolute md:top-[30%] md:left-20 md:max-w-md md:text-white md:py-0 md:px-0">
                 <h1 className="font-semibold text-2xl md:text-3xl  md:drop-shadow-xl">
                   {slide.title}
                 </h1>
@@ -62,6 +63,31 @@ export default function Home() {
           </SwiperSlide>
         ))}
       </Swiper>
+      <motion.div
+        initial={{ opacity: 0, y: -100 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1, delay: 0.5, ease: "easeOut" }}
+        viewport={{ once: false, amount: 0.2 }}
+        className="flex flex-col justify-center items-center w-full mt-60"
+      >
+        <h2 className="font-bold md:text-3xl">About Us</h2>
+        <div className="w-full text-center px-2 mt-3 md:max-w-2xl">
+          <p className="font-medium md:text-lg">
+            Welcome to Bakir Guitar Official, a guitar store dedicated to
+            providing high-quality musical instruments for musicians of all
+            levels. We offer a wide selection of guitars, including acoustic,
+            electric, classic and acoustic electric models from top brands.
+            Committed to delivering the best service, we not only sell guitars
+            but also provide courses for those who want to learn and improve
+            their guitar-playing skils. Addicionally, we ensure that every
+            product we sell undergoes a thorough quality check, so our customers
+            get the best playing experience. We belive that music is a part of
+            life, and Bakir Guitar Official is here to accompany every step of
+            your musical journey with high-quality products, affordable prices,
+            and profesional service.
+          </p>
+        </div>
+      </motion.div>
     </>
   );
 }
